57c2d77022ca5bd77d6b3c65986761212ac40ba6
Tu Mai Anh Do <tudo@isi.edu>
2021-04-19 14:00:00 -0700

    Update wrapper script

--- data/workflows/simple-gpu/bin/wrapper.sh
+++ data/workflows/simple-gpu/bin/wrapper.sh
@@ -1,8 +1,22 @@
 #!/bin/bash -l
+# Loaded Modules
+module purge
+module load cgpu
+module load gcc
+module load cuda/10.2.89
+module load openmpi
+module load python
+module load pytorch
+export PATH="${PYTHONUSERBASE}/bin:${PATH}"
+module load PrgEnv-gnu
+module load craype-haswell
+
+# Dynamic linking
+export CRAYPE_LINK_TYPE=dynamic
 
 old_dir=`pwd`
 cd $PEGASUS_SCRATCH_DIR
 
-srun -G 2 -n $PEGASUS_CORES --gpus-per-task=1 --gpu-bind=map_gpu:0,1 --hint=nomultithread echo "Hello World!!!" > output
+srun -G ${PEGASUS_NUM_WORKERS} -n ${PEGASUS_NUM_WORKERS} --cpus-per-task=2 --gpus-per-task=1 --gpu-bind=map_gpu:0,1 --hint=nomultithread echo "Hello world!!!" > output
 
 cd $old_dir
