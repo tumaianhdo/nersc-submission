0390b40cc500d1c692a9c4987ebe09c51e2132e7
Tu Mai Anh Do <tudo@isi.edu>
2021-04-19 17:56:11 -0700

    Update python test script

--- data/workflows/simple-gpu/input/test.py
+++ data/workflows/simple-gpu/input/test.py
@@ -1,12 +1,14 @@
 import os
-# import torch
+import torch
+from mpi4py import MPI
 
 if __name__ == "__main__":
-    # print(f"torch.cuda.is_available() = {torch.cuda.is_available()}")
-    # print(f"torch.cuda.current_device() = {torch.cuda.current_device()}")
-    # print(f"torch.cuda.device(0) = {torch.cuda.device(0)}")
-    # print(f"torch.cuda.device_count() = {torch.cuda.device_count()}")
-    # print(f"torch.cuda.get_device_name(0) = {torch.cuda.get_device_name(0)}")
-    # print(f'torch.device("cuda") = {torch.device("cuda")}')
-    print(f"CPUs = {os.sched_getaffinity(0)}")
+	worker_id = MPI.COMM_WORLD.Get_rank()
+    print(f"Worker {worker_id}: torch.cuda.is_available() = {torch.cuda.is_available()}")
+    print(f"Worker {worker_id}: torch.cuda.current_device() = {torch.cuda.current_device()}")
+    print(f"Worker {worker_id}: torch.cuda.device(0) = {torch.cuda.device(0)}")
+    print(f"Worker {worker_id}: torch.cuda.device_count() = {torch.cuda.device_count()}")
+    print(f"Worker {worker_id}: torch.cuda.get_device_name(0) = {torch.cuda.get_device_name(0)}")
+    print(f'Worker {worker_id}: torch.device("cuda") = {torch.device("cuda")}')
+    print(f"Worker {worker_id}: CPUs = {os.sched_getaffinity(0)}")
     main()
