3bdd9411427cb0a721201f1b4be23954259717c8
Tu Mai Anh Do <tudo@isi.edu>
2021-04-26 16:55:50 -0700

    Wrapper script

---
+++ data/workflows/galaxy/bin/wrapper.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+module purge
+module load PrgEnv-gnu
+module load craype-haswell
+module load python
+module load pytorch
+export PATH="${PYTHONUSERBASE}/bin:${PATH}"
+
+# Dynamic linking
+export CRAYPE_LINK_TYPE=dynamic
+
+old_dir=`pwd`
+cd $PEGASUS_SCRATCH_DIR
+
+srun -n 1 python create_dataset.py $@
+
+cd $old_dir
