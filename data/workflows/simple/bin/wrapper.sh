33fd3136661ec6163c0daa541aac5e24f16eb316
Tu Mai Anh Do <tudo@isi.edu>
2021-04-27 16:48:35 -0700

    Update wrapper script

--- data/workflows/simple/bin/wrapper.sh
+++ data/workflows/simple/bin/wrapper.sh
@@ -12,6 +12,9 @@ export CRAYPE_LINK_TYPE=dynamic
 old_dir=`pwd`
 cd $PEGASUS_SCRATCH_DIR
 
+echo $(which python)
+python -c "import cv2"
+srun -n $PEGASUS_CORES python -c "import cv2"
 srun -n $PEGASUS_CORES python $@ > output
 
 cd $old_dir
