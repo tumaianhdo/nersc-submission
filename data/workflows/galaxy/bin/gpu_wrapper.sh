05ab33c7b48f994e96bdf8ab1e8c415bac7e9af9
Tu Mai Anh Do <tudo@isi.edu>
2021-04-28 15:56:08 -0700

    Wrapper to run python script on GPUs

---
+++ data/workflows/galaxy/bin/gpu_wrapper.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -l
+# Loaded Modules
+module purge
+module load cgpu
+module load gcc
+module load cuda/10.2.89
+module load openmpi
+module load python
+module load pytorch
+export PYTHONUSERBASE="${USER_HOME}/.local/cori/pytorch1.7.1-gpu"
+export PATH="${PYTHONUSERBASE}/bin:${PATH}"
+module load PrgEnv-gnu
+module load craype-haswell
+
+# Dynamic linking
+export CRAYPE_LINK_TYPE=dynamic
+
+old_dir=`pwd`
+cd $PEGASUS_SCRATCH_DIR
+
+GPU_STRING="0"
+for (( i=1; i<${PEGASUS_NUM_GPUS}; i++ )); do
+    GPU_STRING+=",${i}"
+done
+srun -G ${PEGASUS_NUM_GPUS} -n ${PEGASUS_NUM_GPUS} --cpus-per-task=2 --gpus-per-task=1 --gpu-bind=map_gpu:${GPU_STRING} --hint=nomultithread python $@ >> output
+
+cd $old_dir
