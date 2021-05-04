859b3d58d6b0a36494d7dfd133728dc0f0f3a09e
Tu Mai Anh Do <tudo@isi.edu>
2021-05-04 09:29:05 -0700

    Add log information

--- data/workflows/galaxy/bin/eval_model_vgg16.py
+++ data/workflows/galaxy/bin/eval_model_vgg16.py
@@ -182,6 +182,14 @@ def main():
     best_params = eval(f)
     best_params = best_params["params"]
 
+    print(f"torch.cuda.is_available() = {torch.cuda.is_available()}")
+    print(f"torch.cuda.current_device() = {torch.cuda.current_device()}")
+    print(f"torch.cuda.device(0) = {torch.cuda.device(0)}")
+    print(f"torch.cuda.device_count() = {torch.cuda.device_count()}")
+    print(f"torch.cuda.get_device_name(0) = {torch.cuda.get_device_name(0)}")
+    print(f'torch.device("cuda") = {torch.device("cuda")}')
+    print(f"CPUs = {os.sched_getaffinity(0)}")
+    
     test_model(best_params)
 
     exec_time = time.time() - start
